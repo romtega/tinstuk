1024f1ae690790c1701b38207fd64fa0f5b91e92
Roman <romtega88@gmail.com>
2016-03-01 15:19:21 -0600

    Improved Index Design

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 	def create
 		auth = request.env["omniauth.auth"]
 		session[:omniauth] = auth
-		user = User.sign_in_from_facebook(auth)
+		user = User.sign_in_from_omniauth(auth)
 		session[:user_id] = user.id
 		redirect_to root_url, notice: "Signed In"
 	end
